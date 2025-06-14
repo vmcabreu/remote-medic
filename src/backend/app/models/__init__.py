659b688ce98b4dd812da52860a1349d636344ae0
vmcabreu <victormanueltoa@gmail.com>
2025-06-14 11:23:30 +0100

    added patient to models init

--- src/backend/app/models/__init__.py
+++ src/backend/app/models/__init__.py
@@ -2,8 +2,10 @@ from app.extensions import db
 
 # Importa todos los modelos aquí para que queden registrados
 from .user import User
+from .patients import Patient
 
 # Opcional: exporta en __all__ para importaciones limpias
 __all__ = [
     "User",
+    "Patient"
 ]
--- src/backend/app/models/patients.py
+++ src/backend/app/models/patients.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from app.extensions import db
 
-class User(db.Model):
-    __tablename__ = "users"
+class Patient(db.Model):
+    __tablename__ = "patients"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
